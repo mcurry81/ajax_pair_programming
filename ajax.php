9939711e83a7ab393fac04c2f0e48266b8d291ae
mcurry81 <mcurry4@mail.greenriver.edu>
2015-03-17 00:21:41 -0700

    ajax pair program
    final commit

---
+++ ajax.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+$array = [
+    "CSS" => "Cascading Style Sheets",
+    "HTML" => "HyperText Markup Language",
+    "PHP" => "Hypertext Preprocessor",
+    "XML" => "Extensible Markup Language",
+    "JS" => "JavaScript",
+    "SQL" => "Structured Query Language",
+];
+
+echo $array[$_POST['word']];
+
+?>
